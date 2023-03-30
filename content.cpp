5edade9130fbfeff67b3a19b962abf9db66c0ca6
Balogun Kayode <kayodefredrickbalogun@gmail.com>
2023-03-30 04:17:45 +0000

    add code in files types: .tsx remove code in files types: ,.tsx,.gradle,.bzl,.json,.pro,.xml,.java,.txt,.cpp,.h,.properties,.bat

--- content.cpp
+++ content.cpp
@@ -2,6 +2,5 @@
 using namespace std;
 
 int main() {
-        cout << "cokti";
 return 0;
 }
--- content.java
+++ content.java
@@ -1,5 +1,4 @@
 public class Cddeoy {
     public static void main() {
-        System.out.println("odkrp");
     }
 }
