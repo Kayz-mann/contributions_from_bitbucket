2183b923f23484496e0549e6d62885f74a8ff7c9
Balogun Kayode <kayodefredrickbalogun@gmail.com>
2023-03-30 03:36:31 +0000

    add code in files types: .json,,.tsx,.gradle,.bzl,.pro,.xml,.java,.txt,.cpp,.h,.properties,.bat,.ts remove code in files types: .json,.tsx,.ts

--- content.cpp
+++ content.cpp
@@ -2,5 +2,6 @@
 using namespace std;
 
 int main() {
+        cout << "xaemu";
 return 0;
 }
--- content.java
+++ content.java
@@ -1,4 +1,5 @@
-public class Cxwmar {
+public class Cublcy {
     public static void main() {
+        System.out.println("cwosc");
     }
 }
